6dad9288f1ac0ce2d4e91cdbe10f0db6290bdf95
arunkarthi380-design <arunkarthi380@gmail.com>
2025-12-05 09:51:16 +0530

    Delete Voicemaker directory

--- Voicemaker/script.js
+++
@@ -1,6 +0,0 @@
-
-            function speak() {
-                const input = document.getElementById('text').value;
-                const utterance = new SpeechSynthesisUtterance(input);
-                speechSynthesis.speak(utterance);
-            }
